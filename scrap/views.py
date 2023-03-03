35ae2c4198a9a7f530f0c8768868463af61737ba
sachin-khatrani <sachinkhatrani25@gmail.com>
2023-03-04 01:25:34 +0530

    added changes

--- scrap/views.py
+++ scrap/views.py
@@ -163,4 +163,217 @@ def mooc_based_masters_degree(request):
     template = loader.get_template('mooc-based-masters-degree.html')
     return HttpResponse(template.render())
 
+def dhawal(request):
+    template = loader.get_template('dhawal.html')
+    return HttpResponse(template.render())
+
+def heba(request):
+    template = loader.get_template('heba.html')
+    return HttpResponse(template.render())
+
+def pat_bowden(request):
+    template = loader.get_template('pat-bowden.html')
+    return HttpResponse(template.render())
+
+def manoel(request):
+    template = loader.get_template('manoel.html')
+    return HttpResponse(template.render())
+
+def ruima(request):
+    template = loader.get_template('ruima.html')
+    return HttpResponse(template.render())
+
+def ivy_league_moocs(request):
+    template = loader.get_template('ivy-league-moocs.html')
+    return HttpResponse(template.render())
+
+def collections(request):
+    template = loader.get_template('collections.html')
+    return HttpResponse(template.render())
+
+def sustainability_online_courses(request):
+    template = loader.get_template('sustainability-online-courses.html')
+    return HttpResponse(template.render())
+
+def top_free_online_courses(request):
+    template = loader.get_template('top-free-online-courses.html')
+    return HttpResponse(template.render())
+
+def salesforce(request):
+    template = loader.get_template('salesforce.html')
+    return HttpResponse(template.render())
+
+def google(request):
+    template = loader.get_template('google.html')
+    return HttpResponse(template.render())
+
+def smithsonian(request):
+    template = loader.get_template('smithsonian.html')
+    return HttpResponse(template.render())
+
+def united_nations(request):
+    template = loader.get_template('united-nations.html')
+    return HttpResponse(template.render())
+
+def ibm(request):
+    template = loader.get_template('ibm.html')
+    return HttpResponse(template.render())
+
+def british_council(request):
+    template = loader.get_template('british-council.html')
+    return HttpResponse(template.render())
+
+def institutions(request):
+    template = loader.get_template('institutions.html')
+    return HttpResponse(template.render())
+
+def linuxfoundation(request):
+    template = loader.get_template('linuxfoundation.html')
+    return HttpResponse(template.render())
+
+def amazon(request):
+    template = loader.get_template('amazon.html')
+    return HttpResponse(template.render())
+
+def microsoft(request):
+    template = loader.get_template('microsoft.html')
+    return HttpResponse(template.render())
+
+def mit(request):
+    template = loader.get_template('mit.html')
+    return HttpResponse(template.render())
+
+def stanford(request):
+    template = loader.get_template('stanford.html')
+    return HttpResponse(template.render())
+
+def gatech(request):
+    template = loader.get_template('gatech.html')
+    return HttpResponse(template.render())
+
+def cornell(request):
+    template = loader.get_template('cornell.html')
+    return HttpResponse(template.render())
+
+def purdue(request):
+    template = loader.get_template('purdue.html')
+    return HttpResponse(template.render())
+
+def edinburgh(request):
+    template = loader.get_template('edinburgh.html')
+    return HttpResponse(template.render())
+
+def iitm(request):
+    template = loader.get_template('iitm.html')
+    return HttpResponse(template.render())
+
+def rice(request):
+    template = loader.get_template('rice.html')
+    return HttpResponse(template.render())
+
+def umich(request):
+    template = loader.get_template('umich.html')
+    return HttpResponse(template.render())
+
+def penn(request):
+    template = loader.get_template('penn.html')
+    return HttpResponse(template.render())
+
+def columbia(request):
+    template = loader.get_template('columbia.html')
+    return HttpResponse(template.render())
+
+def duke(request):
+    template = loader.get_template('duke.html')
+    return HttpResponse(template.render())
+
+def harvard(request):
+    template = loader.get_template('harvard.html')
+    return HttpResponse(template.render())
+
+def universities(request):
+    template = loader.get_template('universities.html')
+    return HttpResponse(template.render())
+
+def iit_kharagpur(request):
+    template = loader.get_template('iit-kharagpur.html')
+    return HttpResponse(template.render())
+
+def cs(request):
+    template = loader.get_template('cs.html')
+    return HttpResponse(template.render())
+
+def best_courses(request):
+    template = loader.get_template('best-courses.html')
+    return HttpResponse(template.render())
+
+def business(request):
+    template = loader.get_template('business.html')
+    return HttpResponse(template.render())
+
+def subjects(request):
+    template = loader.get_template('subjects.html')
+    return HttpResponse(template.render())
+
+def udemy(request):
+    template = loader.get_template('udemy.html')
+    return HttpResponse(template.render())
+
+def providers(request):
+    template = loader.get_template('providers.html')
+    return HttpResponse(template.render())
+
+def linkedin_learning(request):
+    template = loader.get_template('linkedin-learning.html')
+    return HttpResponse(template.render())
+
+def skillshare(request):
+    template = loader.get_template('skillshare.html')
+    return HttpResponse(template.render())
+
+def edx(request):
+    template = loader.get_template('edx.html')
+    return HttpResponse(template.render())
+
+def udacity(request):
+    template = loader.get_template('udacity.html')
+    return HttpResponse(template.render())
+
+def futurelearn(request):
+    template = loader.get_template('futurelearn.html')
+    return HttpResponse(template.render())
+
+def swayam(request):
+    template = loader.get_template('swayam.html')
+    return HttpResponse(template.render())
+
+def coursera(request):
+    template = loader.get_template('coursera.html')
+    return HttpResponse(template.render())
+
+def about(request):
+    template = loader.get_template('about.html')
+    return HttpResponse(template.render())
+
+def help(request):
+    template = loader.get_template('help.html')
+    return HttpResponse(template.render())
+
+def signup(request):
+    template = loader.get_template('signup.html')
+    return HttpResponse(template.render())
+
+def login(request):
+    template = loader.get_template('login.html')
+    return HttpResponse(template.render())
+
+def contact(request):
+    template = loader.get_template('contact.html')
+    return HttpResponse(template.render())
+
+def lists(request):
+    template = loader.get_template('lists.html')
+    return HttpResponse(template.render())
+
+
 
